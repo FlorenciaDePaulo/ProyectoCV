d9edc8b8c7abb553819d16d5120c47639a8f492b
FlorenciaDePaulo <124175302+FlorenciaDePaulo@users.noreply.github.com>
2023-02-26 01:08:57 -0300

    Add files via upload
    Argentina Programa 4.0 Entrega#1 CV - De Paulo Florencia

---
+++ Bienvenida.js
@@ -0,0 +1,27 @@
+prompt("Hola visitante, quieres ver el trabajo que he preparado?");
+
+
+
+// const menu = document.querySelector(".menu");
+// const openMenuBtn = document.querySelector(".open-menu");
+// const closeMenuBtn = document.querySelector(".close-menu");
+// function toggleMenu() {
+//   menu.classList.toggle("menu_opened");
+// }
+
+// openMenuBtn.addEventListener("click", toggleMenu);
+// closeMenuBtn.addEventListener("click", toggleMenu);
+
+// const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+// menuLinks.forEach(menuLink =>{
+//   menuLink.addEventListener("click", function(){
+//     menu.classList.remove("menu_opened")
+//   })
+// })
+
+
+
+
+
+
+
